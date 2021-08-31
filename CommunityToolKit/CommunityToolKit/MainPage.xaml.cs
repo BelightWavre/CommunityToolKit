de2476fcc8cd0fe20e562aca872e454df76dcd15
Rus.Wavre <Rus.Wavre@gmail.com>
2021-08-31 10:20:48 +0200

    Add project files.

---
+++ CommunityToolKit/CommunityToolKit/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CommunityToolKit
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private int count = 10;
+        public  int Count
+        {
+            get => count;
+            set
+            {
+                count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+        public ICommand PressedCommand { get => new Command(() => Count++); }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
